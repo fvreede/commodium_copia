5d822ca39431ea8de9fd66c9c7cbdfb3adadb12a
Fabio Vreede <8494527+fvreede@users.noreply.github.com>
2025-01-04 14:25:31 +0100

    Update GuestLayout.vue

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,16 +5,16 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        class="flex min-h-screen flex-col items-center justify-center bg-gray-50 pt-6 sm:pt-0"
     >
         <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+            <Link href="/" class="transition-transform duration-300 hover:scale-105">
+                <ApplicationLogo />
             </Link>
         </div>
 
         <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
+            class="mt-6 w-full max-w-sm bg-white px-6 py-8 shadow-lg rounded-lg border border-gray-200"
         >
             <slot />
         </div>

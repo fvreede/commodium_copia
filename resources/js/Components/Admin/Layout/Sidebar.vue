914ecc9b7a7dd5635e59db48630b445b6c1cc370
Fabio Vreede <8494527+fvreede@users.noreply.github.com>
2025-02-01 13:45:28 +0100

    Translated sidebar labels to Dutch

--- resources/js/Components/Admin/Layout/Sidebar.vue
+++ resources/js/Components/Admin/Layout/Sidebar.vue
@@ -17,7 +17,7 @@ import { HomeIcon, UsersIcon, Cog6ToothIcon, Square3Stack3DIcon, QueueListIcon  
 
             <div class="pt-4">
                 <div class="px-4 text-xs font-semibold text-gray-500 uppercase tracking-wider">
-                    User Management
+                    Gebruikersbeheer
                 </div>
                 <Link 
                     :href="route('admin.users.index')"
@@ -25,13 +25,13 @@ import { HomeIcon, UsersIcon, Cog6ToothIcon, Square3Stack3DIcon, QueueListIcon  
                     :class="{ 'bg-gray-100': route().current('admin.users.*') }"
                 >
                     <UsersIcon class="w-5 h-5 mr-3" />
-                    Users
+                    Gebruikers
                 </Link>
             </div>
 
             <div class="pt-4">
                 <div class="px-4 text-xs font-semibold text-gray-500 uppercase tracking-wider">
-                    Catalog Structure
+                    Catalogusstructuur
                 </div>
                 <Link
                     :href="route('admin.categories.index')"
@@ -39,7 +39,7 @@ import { HomeIcon, UsersIcon, Cog6ToothIcon, Square3Stack3DIcon, QueueListIcon  
                     :class="{ 'bg-gray-100': route().current('admin.categories.*') }"
                 >
                     <Square3Stack3DIcon class="w-5 h-5 mr-3" />
-                    Categories
+                    Categorieën
                 </Link>
                 <Link 
                     :href="route('admin.subcategories.index')"
@@ -47,7 +47,7 @@ import { HomeIcon, UsersIcon, Cog6ToothIcon, Square3Stack3DIcon, QueueListIcon  
                     :class="{ 'bg-gray-100': route().current('admin.subcategories.*') }"
                 >
                     <QueueListIcon class="w-5 h-5 mr-3" />
-                    Subcategories
+                    Subcategorieën
                 </Link>
             </div>
 
@@ -58,7 +58,7 @@ import { HomeIcon, UsersIcon, Cog6ToothIcon, Square3Stack3DIcon, QueueListIcon  
                     :class="{ 'bg-gray-100': route().current('admin.settings') }"
                 >
                     <Cog6ToothIcon class="w-5 h-5 mr-3" />
-                    Settings
+                    Instellingen
                 </Link>
             </div>
         </nav>
